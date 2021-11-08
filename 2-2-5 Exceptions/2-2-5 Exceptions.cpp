10b1e0528857edc10d889269cd6d291c33524a53
Isabelle Shi <ShiIsab508@GoTVDSB.ca>
2021-11-08 10:54:27 -0500

    Add project files.

---
+++ 2-2-5 Exceptions/2-2-5 Exceptions.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout << "Exercise #1 \n";
+    cout << " \n";
+
+    string name;
+
+    cout << "Enter a name \n";
+    cin >> name;
+
+
+}
